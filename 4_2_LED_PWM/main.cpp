9df1ba733630fa55382f46ff969eee3c2ac6f0d8
ee2405 <Melody90112>
2021-03-02 22:19:36 -0800

    add main.cpp

---
+++ 4_2_LED_PWM/main.cpp
@@ -0,0 +1,16 @@
+#include "mbed.h"
+
+PwmOut PWM1(D6);
+AnalogIn Ain(A0);
+float ADCdata;
+
+int main(void) 
+{
+    while (1) {
+        PWM1.period_ms(500);
+        PWM1 = Ain;
+        ADCdata = Ain;
+        printf("%f\r\n", ADCdata);
+        ThisThread::sleep_for(100ms);
+    }
+}
